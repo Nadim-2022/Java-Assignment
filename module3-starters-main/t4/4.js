052ebbaa63509044fe75c4f26591d6beed34d9b7
iamna <i.am.nadim95@gmail.cojm>
2022-11-21 21:53:33 +0200

    new

---
+++ module3-starters-main/t4/4.js
@@ -0,0 +1,34 @@
+'use strict';
+const students = [
+  {
+    name: 'John',
+    id: '2345768',
+  },
+  {
+    name: 'Paul',
+    id: '2134657',
+  },
+  {
+    name: 'Jones',
+    id: '5423679',
+  },
+];
+
+console.log(students[0])
+
+/*
+const target = document.querySelector('#target');
+
+function creat(array) {
+  let li = document.createElement('option');
+  li.innerText = array;
+  return li;
+}
+
+
+for (const student in students) {
+  target.appendChild(creat(students[student]))
+}
+
+
+ */
