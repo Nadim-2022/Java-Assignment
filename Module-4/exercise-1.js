231269697831610c3c9a44f307da77bfc233d424
iamna <i.am.nadim95@gmail.cojm>
2022-11-28 22:25:21 +0200

    new

--- Module-4/exercise-1.js
+++ Module-4/exercise-1.js
@@ -1,3 +1,16 @@
+/*
 fetch('https://api.tvmaze.com/search/shows?q=girls')
   .then((response) => response.json())
   .then((data) => console.log(data));
+
+ */
+
+const api_url = 'https://api.tvmaze.com/search/shows?q=girls'
+async function getIss(){
+  const response = await fetch(api_url)
+  const data = await response.json();
+  for ( const da of data){
+    document.querySelector('p').innerHTML = data[da]
+  }
+}
+getIss()
